dcd1f4fee7a7e1b0bbcfb4b0c31e7986c012b8f1
bounce <johan@waytogo.se>
2020-02-10 09:47:15 +0100

    changing test to not access main data file

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -6,7 +6,7 @@ describe Library do
     subject {described_class.new}
 
     it 'to see that the library has a list of books' do 
-        expect(subject.collection).to_not eq nil
+        expect(subject.list).to_not eq nil
     end 
 
     it 'to see a list of all books' do
